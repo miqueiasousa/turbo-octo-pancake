127440dfe70404d3d3204cf9a2f8343c1516e61e
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-06-20 21:29:59 -0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <h1 className="font-bold text-9xl">Hello world</h1>
+    </div>
+  )
+}
+
+export default App
