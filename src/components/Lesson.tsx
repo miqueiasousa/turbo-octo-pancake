71c567a998e3229ef3ecbf33325d433782e8541c
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-06-24 21:47:35 -0300

    feat(interface): add group hover

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -17,11 +17,11 @@ export default function Lesson({
   type
 }: LessonProps) {
   return (
-    <a href="">
+    <a href="" className="group">
       <span className="text-gray-300">
         {formatLessonAvailbleAt(availableAt)}
       </span>
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           {isPast(availableAt) ? (
             <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
