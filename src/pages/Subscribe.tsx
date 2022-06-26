fcc369961a5a49a78dad750e5863dd4562942956
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-06-26 20:17:39 -0300

    feat: create Subscribe page

---
+++ src/pages/Subscribe.tsx
@@ -0,0 +1,31 @@
+import Form from '../components/Form'
+import Logo from '../components/Logo'
+
+export default function Subscribe() {
+  return (
+    <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+        <div className="max-w-[640px]">
+          <Logo />
+          <h1 className="mt-8 text-[2.5rem] leading-tight">
+            Construa uma{' '}
+            <strong className="text-blue-500">aplicação completa</strong>, do
+            zero, com <strong className="text-blue-500">React</strong>
+          </h1>
+          <p className="mt-4 text-gray-200 leading-relaxed">
+            Em apenas uma semana você vai dominar na prática uma das tecnologias
+            mais utilizadas e com alta demanda para acessar as melhores
+            oportunidades do mercado.
+          </p>
+        </div>
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+          <strong className="text-2xl mb-6 block">
+            Inscreva-se gratuitamente
+          </strong>
+          <Form />
+        </div>
+      </div>
+      <img className="mt-10" src="/src/assets/code-mockup.png" />
+    </div>
+  )
+}
